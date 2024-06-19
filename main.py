d588304b4d5e606ed7494f6337cd879f350287a0
Santiago Sanchez <ssanchez@kuenta.co>
2024-06-19 16:04:44 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+from database.read_database import ReadImages
+
+
+class AutoLabellingObjectDetect:
+    def __init__(self):
+        self.data = ReadImages()
+
+    def main(self):
+        images, names = self.data.read_images('database/images_with_labels')
+        print(len(images), len(names))
+
+
+auto_labeling = AutoLabellingObjectDetect()
+auto_labeling.main()
