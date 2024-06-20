b09cb7a43d9226a7fd1aa8ac6d549e694df353cd
Santiago Sanchez <ssanchez@kuenta.co>
2024-06-19 21:18:05 -0500

    refactor

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import time
-from database.read_database import ReadImages
-=======
-from database import ReadImages
->>>>>>> a5ca029 (initial commit)
-=======
 from time import time
 import os
 import cv2
 from PIL import Image
-from database.read_database import ReadImages
 from GroundingDINO.groundingdino.util.inference import load_model, load_image, predict, annotate
 import GroundingDINO.groundingdino.datasets.transforms as T
+from database.read_database import ReadImages
 
->>>>>>> 66ff857 (add requirements)
 
 
 class AutoLabellingObjectDetect:
