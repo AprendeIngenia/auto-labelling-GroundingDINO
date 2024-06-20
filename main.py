19f3a8bde7865d32c267964395f2cddd21260739
Santiago Sanchez <ssanchez@kuenta.co>
2024-06-19 21:12:51 -0500

    rebase 5

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import time
 from database.read_database import ReadImages
 =======
 from database import ReadImages
 >>>>>>> a5ca029 (initial commit)
+=======
+from time import time
+import os
+import cv2
+from PIL import Image
+from database.read_database import ReadImages
+from GroundingDINO.groundingdino.util.inference import load_model, load_image, predict, annotate
+import GroundingDINO.groundingdino.datasets.transforms as T
+
+>>>>>>> 66ff857 (add requirements)
 
 
 class AutoLabellingObjectDetect:
@@ -11,7 +22,7 @@ class AutoLabellingObjectDetect:
         self.data = ReadImages()
 
     def main(self):
-        images, names = self.data.read_images('database/images_with_labels')
+        images, names = self.data.read_images('database/untagged_images')
         print(len(images), len(names))
 
         # main process
